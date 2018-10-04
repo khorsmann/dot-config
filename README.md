01ac2229ab92f81be478653984350936a2b3f5d8
Karsten Horsmann <khorsmann@gmail.com>
2018-10-04 16:37:06 +0200

    add files

---
+++ README.md
@@ -0,0 +1,6 @@
+## Dot-Config Folder
+
+Should be become my folder for $HOME/.dot-file templates<br/>
+<br/>
+Symlinks $dot-config/.dot-file to $HOME/.file<br/>
+
---
+++ setup.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+import re
+import os
+import os.path
+from collections import defaultdict
+import pprint
+
+"""
+BLACKLIST = filenames and/or absulute path to folder to ignore them
+"""
+
+HOME = os.environ['HOME']
+MYPATH = os.path.normpath(os.path.dirname(os.path.realpath(__file__)))
+MYNAME = os.path.normpath(os.path.basename(os.path.realpath(__file__)))
+GITFOLDER = [ x[0] for x in os.walk(os.path.join(MYPATH, '.git'))]
+BLACKLIST = set([ MYNAME,
+                '.gitignore',
+                'README.md',
+                ] + GITFOLDER )
+
+def defaultdict_factory(*args, **kwargs):
+    """ Create and return a defaultdict(dict).  """
+    return defaultdict(dict, *args, **kwargs)
+
+
+def addfiles(filedict, root, tfolder, files):
+    """ add Files to Source/Target Dict for ln -s """
+    for s in files:
+        if s in BLACKLIST:
+            continue
+        if root in BLACKLIST:
+            continue
+        t = re.sub(r'^dot-', '.', s)
+        s = os.path.join(root, s)
+        filedict['FILES'][s] = os.path.join(tfolder, t)
+    return filedict
+
+
+def main():
+
+    filedict = defaultdict(defaultdict_factory)
+
+    for root, _, files in os.walk(MYPATH):
+        if files:
+            tfolder = (''.join([HOME, root[len(MYPATH):]]))
+            if tfolder != HOME:
+                if root not in BLACKLIST:
+                    filedict['FOLDERS'][tfolder] = oct(os.stat(root).st_mode & 0o777)
+            filedict = addfiles(filedict, root, tfolder, files)
+
+    # pprint.pprint(filedict)
+
+    # generate folders
+    for folder, oct_rights in sorted(filedict['FOLDERS'].items()):
+        if not os.path.isdir(folder):
+            print('mkdir -p {0}'.format(folder))
+            os.makedirs(folder)
+            print('chmod {0} {1}'.format(oct_rights[2:], folder))
+            os.chmod(folder,int(oct_rights,8))
+
+    # generate symlinks
+    for src, dest in sorted(filedict['FILES'].items()):
+        if os.path.isfile(dest) and not os.path.islink(dest):
+            bak = '.'.join([dest, 'bak'])
+            print('mv {0} {1}'.format(dest, bak))
+            os.rename(dest, bak)
+        if not os.path.islink(dest):
+            print('ln -s {0} {1}'.format(src, dest))
+            os.symlink(src, dest)
+
+
+if __name__ == '__main__':
+    main()
