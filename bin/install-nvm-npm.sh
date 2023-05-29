17f86fc8bf434f73cd9dd13c7d8a206a0702a60d
Karsten Horsmann <khorsmann@gmail.com>
2023-05-29 23:02:27 +0200

    add pyenv-update, add lazygit

--- bin/install-nvm-npm.sh
+++ bin/install-nvm-npm.sh
@@ -6,4 +6,6 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 #  args:
 #    creates: "{{ ansible_env.HOME }}/.nvm/nvm.sh"
 nvm install v18.16.0
+npm install -g bash-language-server
 npm install -g @ansible/ansible-language-server
+npm install -g yaml-language-server@next
--- bin/install-pyenv.sh
+++ bin/install-pyenv.sh
@@ -4,3 +4,5 @@ sudo apt update; sudo apt install build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev curl \
 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+echo "add pyenv-update"
+git clone https://github.com/pyenv/pyenv-update.git $(pyenv root)/plugins/pyenv-update
