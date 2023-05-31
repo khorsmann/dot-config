db6bf8ec4ea9414211ffb133cdf07aa9a7ca390f
Karsten Horsmann <khorsmann@gmail.com>
2023-05-31 14:00:26 +0200

    add themes for alacritty, add yq. todo fix ugly colors in screen if helix is used

---
+++ bin/install-alacritty-themes.sh
@@ -0,0 +1,8 @@
+# We use Alacritty's default Linux config directory as our storage location here.
+mkdir -p ~/.config/alacritty/themes
+git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
+# https://github.com/rajasegar/alacritty-themes/ 
+echo "or use npm to install over 200+ themes"
+npm -i -g alacritty-themes
+echo "usage 'alacritty-themes'
+
