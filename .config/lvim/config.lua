d4af7f4f0d90434a3b5c3765ab03560435efb8ee
Karsten Horsmann <khorsmann@gmail.com>
2023-06-27 13:13:21 +0200

    wip lunarvim

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -2,16 +2,36 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
-lvim.use_icons = true
+-- 
+-- config most copied from https://github.com/rebuilt/lvim/blob/master/config.lua
 vim.opt.guicursor = "" -- bold Cursor everytime/everymode
 vim.opt.number = true
 vim.opt.colorcolumn = "80"
+
+lvim.use_icons = true -- needs nerdfonts
+lvim.line_wrap_cursor_movement = false
+-- lvim.builtin.alpha.active = false
+lvim.builtin.lir.active = false
+-- lvim.builtin.comment.active = false
+lvim.builtin.terminal.active = true
+lvim.builtin.project.active = true
+lvim.builtin.indentlines.active = false
+lvim.builtin.treesitter.ignore_install = { "kotlin" }
+lvim.builtin.treesitter.ensure_installed = { "python", "yaml" }
+lvim.builtin.treesitter.highlight.enabled = true
+lvim.builtin.treesitter.highlight.enable = true
+lvim.builtin.nvimtree.setup.open_on_setup = true
+lvim.builtin.treesitter.highlight.additional_vim_regex_highlighting = false
+lvim.builtin.treesitter.playground.enable = true
+-- lvim.builtin.treesitter.textsubjects.enable = true
+-- lvim.builtin.treesitter.textsubjects.keymaps[";"] = "textsubjects-big"
+-- lvim.builtin.treesitter.textsubjects.keymaps["."] = "textsubjects-smart";
+lvim.lsp.document_highlight = true
 
 -- plugins
 -- ==========================================
 require("user.plugins").setup()
 
--- Autocmd commands
 -- -- Persistent Folds
 local augroup = vim.api.nvim_create_augroup
 local save_fold = augroup("Persistent Folds", { clear = true })
@@ -60,3 +80,6 @@ vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave" }, {
   end,
 })
 
+-- Autocmd commands
+lvim.builtin.cmp.formatting.source_names["copilot"] = "(Copilot)"
+table.insert(lvim.builtin.cmp.sources, 1, { name = "copilot" })
